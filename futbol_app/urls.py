5d30b9662f77481544fa36bbe8bab991abe5d4c0
andresscq <ivancriollo3@gmail.com>
2025-06-22 22:29:23 -0500

    Segundo commit: Creacion de futbol_app

--- futbol_app/urls.py
+++ futbol_app/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
+ # futbol_app/urls.py
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path('admin/', admin.site.urls),
+    path('', include('futbol.urls')),  # Esto conecta las URLs de la app 'futbol'
 ]
